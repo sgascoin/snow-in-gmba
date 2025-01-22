51a55686a0d00590f95547a775e735246cfdeb37
Simon Gascoin <29677722+sgascoin@users.noreply.github.com>
2025-01-22 14:09:15 +0100

    move elevation loop to end

--- snowProba.js
+++ snowProba.js
@@ -6,12 +6,26 @@
 var config = {
   name:'snowProba', 
   ymin:2000, 
-  ymax:2022
+  ymax:2023
 };
 
 // input datasets
-var gmba = ee.FeatureCollection("users/sgascoin/GMBA_lite");
+var gmba0 = ee.FeatureCollection("users/sgascoin/GMBA_lite");
 var dem250 = ee.Image("USGS/GMTED2010_FULL");
+
+// function to set a (multi-)polygon to non-geodesic 
+var setNoGeodesic = function(ft){
+  var coords = ft.geometry().coordinates();
+  var poly = ee.Geometry.MultiPolygon({
+    coords:coords,
+    geodesic: false
+  });
+ return ft.setGeometry(poly);
+};
+
+// to avoid projection issues with SR-ORG:6974 PLANAR (MOD10A1),
+// set GMBA to non-geodesic 
+var gmba = gmba0.map(setNoGeodesic);
 
 // collection of interest
 var coi = ee.ImageCollection("MODIS/061/MOD10A1").select('NDSI_Snow_Cover');
@@ -64,23 +78,42 @@ var low0 = 0;
 // max elevation of the lower bound
 var low1 = 9000;
 
+// elevation band width
+var step = 500;
+
 // output filename prefix
 var name0 = config.name;
+
+// all elevations
+var elevParam = {low:low0, up:low1};
+
+// adjust output filename
+var zmin = ee.Number(elevParam.low).format('%04d');
+var zmax = ee.Number(elevParam.up).format('%04d');
+config.name = ee.String(name0+'_').cat(zmin).cat(zmax).cat('_');
+
+// create DEM mask
+var demMask = dem.gte(elevParam.low).and(dem.lt(elevParam.up));
+
+// run computations
+addToGmba.addToGmba(config,coi,gmba,getVoi);
 
 // elevation band loop (client-side)
 while (low0 < low1) {
   // define elevation range
-  var elevParam = {low:low0, up:low0 + 500};
+  var elevParam = {low:low0, up:low0 + step};
   
   // adjust output filename
-  config.name = name0+'_'+elevParam.low+elevParam.up+'_';
-  
+  var zmin = ee.Number(elevParam.low).format('%04d');
+  var zmax = ee.Number(elevParam.up).format('%04d');
+  config.name = ee.String(name0+'_').cat(zmin).cat(zmax).cat('_');
+
   // create DEM mask
   var demMask = dem.gte(elevParam.low).and(dem.lt(elevParam.up));
-  
+
   // run computations
   addToGmba.addToGmba(config,coi,gmba,getVoi);
   
   // increment lower elevation
-  low0 = low0 + 500;
+  low0 = low0 + step;
 }
